ac30e9c8a65a766320a49be10d614ccd7a6db499
Vyacheslav Knyazev <ks38@mail.ru>
2022-11-25 14:54:43 +0300

    Homework2 done

---
+++ lesson2/tasks/Program.cs
@@ -0,0 +1,50 @@
+﻿class HomeWork2
+{
+    static void Main()
+    {
+        Console.Write("Enter number for task10 -> ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(Task10(number));
+
+        Console.Write("Enter number for task13 -> ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(Task13(number2));
+
+        Console.Write("Enter number of day of week for task10 -> ");
+        int number3 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(Task15(number3));
+    }
+
+    static int Task10(int num)
+    {
+        int rezult = -1;
+        if (num < 999 && num > 100)
+        {
+            rezult = (num / 10) % 10;
+        }
+        return rezult;
+    }
+
+    static int Task13(int num)
+    {
+        if (num < 100)
+        {
+            Console.WriteLine("There is no third number");
+        }
+        else
+        {
+            while (!(num / 1000 == 0))
+            {
+                num /= 10;
+            }
+        }
+        return num %= 10;
+    }
+
+    static bool Task15(int day)
+    {
+        if (day == 6 || day == 7) return true;
+        return false;
+    }
+
+}
