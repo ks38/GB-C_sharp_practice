0ffe1eee7e8572686adc142235e6df2b58f1cf0f
Vyacheslav Knyazev - PC <ks38@mail.ru>
2022-12-05 22:43:22 +0300

    Task 34, 36 - done

---
+++ lesson5/Homework/task34/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] array = new int[10];
+RandomizeArray(array);
+PrintArray(array);
+EvenCount(array);
+
+void RandomizeArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.WriteLine($"[{string.Join(", ", arr)}]");
+}
+
+void EvenCount(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    System.Console.WriteLine("Even elements: " + count + " pcs");
+}
---
+++ lesson5/Homework/task36/Program.cs
@@ -0,0 +1,32 @@
+﻿const int SIZE = 25;
+int[] arr = new int[SIZE];
+RandomizeArray(arr);
+PrintArray(arr);
+SummCount(arr);
+
+void RandomizeArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.WriteLine($"[{string.Join(", ", arr)}]");
+}
+
+void SummCount(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 != 0)
+        {
+            sum += arr[i];
+        }
+    }
+    System.Console.WriteLine("Summ of elements: " + sum);
+}
+
