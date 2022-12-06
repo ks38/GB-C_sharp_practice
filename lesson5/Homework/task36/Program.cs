a3b94823af1aef54decd646062eb48673063cc2e
Vyacheslav Knyazev - PC <ks38@mail.ru>
2022-12-06 13:14:37 +0300

    Homework 5 - done

--- lesson5/Homework/task36/Program.cs
+++ lesson5/Homework/task36/Program.cs
@@ -22,7 +22,7 @@ void SummCount(int[] arr)
     int sum = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i] % 2 != 0)
+        if (i % 2 != 0)
         {
             sum += arr[i];
         }
--- lesson5/Homework/task38/Program.cs
+++ lesson5/Homework/task38/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const int SIZE = 6;
+
+int[] array = new int[SIZE];
+
+RandomizeArray(array);
+PrintArray(array);
+(int max, int min) = GetSubstractionMaxAndMin(array);
+System.Console.WriteLine($"max = {max}, min = {min}. Substraction = {max - min}");
+
+int[] RandomizeArray(int[] arr)
+{
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(0, 101);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.WriteLine($"[{string.Join(", ", arr)}]");
+}
+
+(int, int) GetSubstractionMaxAndMin(int[] array)
+{
+    int max = array[0];
+    int min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return (max, min);
+}
