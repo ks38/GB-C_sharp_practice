63d551c8c8a29140c9441218ec7c04c0b205a0cd
Vyacheslav Knyazev - PC <ks38@mail.ru>
2022-12-08 22:31:28 +0300

    Homework 6 - done

---
+++ lesson6/Homework/task41/Program.cs
@@ -0,0 +1,24 @@
+﻿Counting();
+
+void Counting()
+{
+    int count = 0;
+    while (true)
+    {
+        Console.Write("Enter number or \"stop\": ");
+        string word = Console.ReadLine()!.ToLower();
+        if (word == "stop")
+        {
+            break;
+        }
+        else
+        {
+            int number = Convert.ToInt32(word);
+            if (number > 0)
+            {
+                count++;
+            }
+        }
+    }
+    Console.WriteLine($"You have entered {count} positive numbers");
+}
---
+++ lesson6/Homework/task43/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Intersection(b1, k1, b2, k2);
+
+void Intersection(double b1, double k1, double b2, double k2)
+{
+    double x = Convert.ToDouble((b2 - b1) / (k1 - k2));
+    double y = Convert.ToDouble(k2 * x + b2);
+    x = Math.Round(x, 3);
+    y = Math.Round(y, 3);
+
+    Console.WriteLine($"The point of intersection of two straight lines is {x} and {y}");
+}
